e5a28e2c1255f57e67a33bbab0c9ebeb803a3bca
SambaLim <limsungho07@hanmail.net>
2019-12-24 22:39:01 +0900

    profile pre

--- front/.next/server/static/development/pages/profile.js
+++ front/.next/server/static/development/pages/profile.js
@@ -88,7 +88,7 @@ module.exports =
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 4);
+/******/ 	return __webpack_require__(__webpack_require__.s = 5);
 /******/ })
 /************************************************************************/
 /******/ ({
@@ -104,25 +104,185 @@ module.exports =
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var antd__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! antd */ "antd");
+/* harmony import */ var antd__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(antd__WEBPACK_IMPORTED_MODULE_1__);
 var _jsxFileName = "C:\\playground\\reactNodebird\\nodebird\\front\\pages\\profile.js";
 var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
 
 
 const Profile = () => {
   return __jsx("div", {
     __source: {
       fileName: _jsxFileName,
-      lineNumber: 5
+      lineNumber: 6
     },
     __self: undefined
-  }, "\uB0B4 \uD504\uB85C\uD544");
+  }, __jsx(antd__WEBPACK_IMPORTED_MODULE_1__["Form"], {
+    style: {
+      marginBottom: '20px',
+      border: '1px solid #d9d9d9',
+      padding: '20px'
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7
+    },
+    __self: undefined
+  }, __jsx(antd__WEBPACK_IMPORTED_MODULE_1__["Input"], {
+    addonBefore: "\uB2C9\uB124\uC784",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 8
+    },
+    __self: undefined
+  }), __jsx(antd__WEBPACK_IMPORTED_MODULE_1__["Button"], {
+    type: "primary",
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    },
+    __self: undefined
+  }, "\uC218\uC815")), __jsx(antd__WEBPACK_IMPORTED_MODULE_1__["List"], {
+    style: {
+      marginBottom: '20px'
+    },
+    grid: {
+      gutter: 4,
+      xs: 2,
+      md: 3
+    },
+    header: __jsx("div", {
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 14
+      },
+      __self: undefined
+    }, "\uD314\uB85C\uC789 \uBAA9\uB85D"),
+    loadMore: __jsx(antd__WEBPACK_IMPORTED_MODULE_1__["Button"], {
+      style: {
+        width: '100%'
+      },
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 15
+      },
+      __self: undefined
+    }, "\uB354\uBCF4\uAE30"),
+    bordered: true,
+    dataSource: ['삼바림', '킹', '임성호오피셜'],
+    renderItem: item => {
+      __jsx(antd__WEBPACK_IMPORTED_MODULE_1__["List"].Item, {
+        style: {
+          marginTop: '20px'
+        },
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 19
+        },
+        __self: undefined
+      }, __jsx(antd__WEBPACK_IMPORTED_MODULE_1__["Card"], {
+        actions: [__jsx(antd__WEBPACK_IMPORTED_MODULE_1__["Icon"], {
+          type: "stop",
+          __source: {
+            fileName: _jsxFileName,
+            lineNumber: 20
+          },
+          __self: undefined
+        })],
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 20
+        },
+        __self: undefined
+      }, __jsx(antd__WEBPACK_IMPORTED_MODULE_1__["Card"].Meta, {
+        description: item,
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 20
+        },
+        __self: undefined
+      })));
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 11
+    },
+    __self: undefined
+  }), __jsx(antd__WEBPACK_IMPORTED_MODULE_1__["List"], {
+    style: {
+      marginBottom: '20px'
+    },
+    grid: {
+      gutter: 4,
+      xs: 2,
+      md: 3
+    },
+    header: __jsx("div", {
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 27
+      },
+      __self: undefined
+    }, "\uD314\uB85C\uC6CC \uBAA9\uB85D"),
+    loadMore: __jsx(antd__WEBPACK_IMPORTED_MODULE_1__["Button"], {
+      style: {
+        width: '100%'
+      },
+      __source: {
+        fileName: _jsxFileName,
+        lineNumber: 28
+      },
+      __self: undefined
+    }, "\uB354\uBCF4\uAE30"),
+    bordered: true,
+    dataSource: ['삼바림', '킹', '임성호오피셜'],
+    renderItem: item => {
+      __jsx(antd__WEBPACK_IMPORTED_MODULE_1__["List"].Item, {
+        style: {
+          marginTop: '20px'
+        },
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 32
+        },
+        __self: undefined
+      }, __jsx(antd__WEBPACK_IMPORTED_MODULE_1__["Card"], {
+        actions: [__jsx(antd__WEBPACK_IMPORTED_MODULE_1__["Icon"], {
+          type: "stop",
+          __source: {
+            fileName: _jsxFileName,
+            lineNumber: 33
+          },
+          __self: undefined
+        })],
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 33
+        },
+        __self: undefined
+      }, __jsx(antd__WEBPACK_IMPORTED_MODULE_1__["Card"].Meta, {
+        description: item,
+        __source: {
+          fileName: _jsxFileName,
+          lineNumber: 33
+        },
+        __self: undefined
+      })));
+    },
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 24
+    },
+    __self: undefined
+  }));
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (Profile);
 
 /***/ }),
 
-/***/ 4:
+/***/ 5:
 /*!********************************!*\
   !*** multi ./pages/profile.js ***!
   \********************************/
@@ -131,6 +291,17 @@ const Profile = () => {
 
 module.exports = __webpack_require__(/*! C:\playground\reactNodebird\nodebird\front\pages\profile.js */"./pages/profile.js");
 
+
+/***/ }),
+
+/***/ "antd":
+/*!***********************!*\
+  !*** external "antd" ***!
+  \***********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = require("antd");
 
 /***/ }),
 
--- front/.next/static/runtime/webpack.js
+++ front/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f90d6262fc0ac5abb442";
+/******/ 	var hotCurrentHash = "7fa486ad9010fe87af51";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
